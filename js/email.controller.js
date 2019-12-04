ca5fa49b80ffcee5c624d4decdcd19e8f8d29443
Gabriel Schaidhauer <gabriel@Gabriels-MacBook-Pro.local>
2019-12-03 21:58:23 -0300

    Adding partial masks

--- js/email.controller.js
+++ js/email.controller.js
@@ -2,21 +2,18 @@ $(document).ready(function () {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
         setEstimate();
-        sendMail(this, 'estimate_form');
-        sendMail(this, 'contact_form');
+        Promise.all([sendMail(this, 'estimate_form'), sendMail(this, 'contact_form')])
+        .then(() => {
+            $.modal.close();
+            alert('success');
+        }).catch(() => {
+            $.modal.close();
+            alert('error');
+        });
+        
     });
 });
 
 function sendMail(form, template) {
-    emailjs.sendForm('gmail', template, form)
-        .then(function (response) {
-            $.modal.close()
-            document.getElementById("contact-form").reset();
-            alert("E-mail enviado, aguarde nosso retorno em breve!");
-        },
-            function (error) {
-                console.log(error);
-                $.modal.close()
-                alert("Ocorreu um erro tente novamente mais tarde");
-            });
+    return emailjs.sendForm('gmail', template, form);
 }
--- js/estimate.controller.js
+++ js/estimate.controller.js
@@ -1,3 +1,10 @@
+// Adding estimate masks
+$(document).ready(function() {
+    $('#start').mask('99/99/9999')
+    $('#end').mask('99/99/9999');
+    $('#members').mask('99');
+})
+
 function toDate(dateStr) {
     var parts = dateStr.split("/")
     return new Date(parts[2], parts[1] - 1, parts[0])
