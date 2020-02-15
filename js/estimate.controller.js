6de0885f92d1dda80745e5d7655ab7bf19b2d23f
Gabriel Schaidhauer <gabriel@Gabriels-MacBook-Pro.local>
2020-02-15 00:35:08 -0300

    Adding new calculation formula

--- js/estimate.controller.js
+++ js/estimate.controller.js
@@ -2,7 +2,12 @@
 $(document).ready(function() {
     $('#start').mask('99/99/9999')
     $('#end').mask('99/99/9999');
-    $('#members').mask('99');
+    $('#members').mask('9');
+    $('#members').bind('input', function(event) {
+        if(event.target.value > 8) {
+            event.target.value = 8;
+        }
+    });
 })
 
 function toDate(dateStr) {
@@ -16,9 +21,32 @@ function daysCount(date1, date2) {
     return diffDays + 1;
 }
 
-function calculateEstimate({ members, days }) {
-    const basePricePerPerson = 140;
-    return (parseInt(members) * parseInt(days)) * parseInt(basePricePerPerson);
+function calculateEstimate({ members, days, startMonth, endMonth }) {
+    const highDemandPrice = 350;
+    const lowDemandPrice = 250;
+    let price = 0;
+    if(isHighDemand(startMonth, endMonth)) {
+        price = highDemandPrice;
+    } else {
+        price = lowDemandPrice;
+    }
+
+    return days * price;
+}
+
+function isHighDemand(startMonth, endMonth) {
+    const highDemand = [1, 6, 7, 8, 11, 12];
+    if(highDemand.some(function(s) {return s === startMonth}) || highDemand.some(function(e) {return e === endMonth})) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getMonth(dateStr) {
+    if(dateStr) {
+        return parseInt(dateStr.split('/')[1]);
+    }
 }
 
 function setEstimate() {
@@ -26,7 +54,7 @@ function setEstimate() {
     const startDateString = $('#start').val();
     const endDateString = $('#end').val();
     const days = daysCount(toDate(endDateString), toDate(startDateString));
-    const estimate = calculateEstimate({members, days});
+    const estimate = calculateEstimate({members, days, startMonth: getMonth(startDateString), endMonth: getMonth(endDateString)});
 
     $('#days').val(days);
     $('#estimate').val(estimate);
